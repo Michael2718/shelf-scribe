7bbcc80e18f153cae640a8d2e2ac549467c7ae8c
Michael <isaevmihail007@gmail.com>
2023-08-18 17:36:21 +0300

    Update SearchDetailsScreen

--- app/src/main/java/com/example/shelf_scribe/ui/screens/search/SearchDetailsScreen.kt
+++ app/src/main/java/com/example/shelf_scribe/ui/screens/search/SearchDetailsScreen.kt
@@ -1,23 +1,35 @@
 package com.example.shelf_scribe.ui.screens.search
 
 import android.content.Context
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -43,6 +55,7 @@ fun SearchDetailsScreen(
                 modifier = modifier
             )
         }
+
         is VolumeRequestStatus.Error -> ErrorScreen(retryAction, Modifier.fillMaxSize())
     }
 }
@@ -63,38 +76,71 @@ fun SearchDetailsScreenContent(
             .padding(dimensionResource(R.dimen.padding_small))
             .verticalScroll(scrollState)
     ) {
-        Card(
-            modifier = Modifier.fillMaxSize(),
-            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        Row(
+            modifier = Modifier.fillMaxWidth()
         ) {
-            if (imageLink == null) {
-                Image(
-                    painter =  painterResource(R.drawable.ic_broken_image),
-                    contentDescription = stringResource(R.string.book_thumbnail_is_missing),
-                    modifier = Modifier
-                        .wrapContentSize()
-                        .fillMaxSize(),
-                    alignment = Alignment.Center,
-                    contentScale = ContentScale.FillWidth
+            ImageCard(
+                imageLink = imageLink,
+                context = context,
+                modifier = Modifier
+//                    .wrapContentWidth()
+                    .wrapContentSize()
+                    .padding(dimensionResource(R.dimen.padding_small))
+            )
+//            Image(imageVector = Icons.Default.AccountBox, contentDescription = null)
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = title,
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.titleLarge
                 )
-            } else {
-                AsyncImage(
-                    model = ImageRequest.Builder(context = context)
-                        .data(imageLink.replace("http", "https"))
-                        .crossfade(true)
-                        .build(),
-                    contentDescription = title,
-                    modifier = Modifier
-                        .wrapContentSize()
-                        .fillMaxSize(),
-                    placeholder = painterResource(R.drawable.loading_img),
-                    error = painterResource(R.drawable.ic_broken_image),
-                    alignment = Alignment.Center,
-                    contentScale = ContentScale.FillWidth
+                Text(
+                    text = volume.volumeInfo.authors[0],
+                    style = MaterialTheme.typography.bodyLarge
                 )
+//                volume.volumeInfo.description?.let { Text(text = it) }
             }
         }
-        Text(text = "${volume.volumeInfo.title} - ${volume.volumeInfo.authors}")
-        volume.volumeInfo.description?.let { Text(text = it) }
+    }
+}
+
+@Composable
+fun ImageCard(
+    imageLink: String?,
+    context: Context,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        shape = RectangleShape,
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        if (imageLink == null) {
+            Image(
+                painter = painterResource(R.drawable.ic_broken_image),
+                contentDescription = stringResource(R.string.book_thumbnail_is_missing),
+                modifier = Modifier,
+                alignment = Alignment.Center,
+                contentScale = ContentScale.FillWidth
+            )
+        } else {
+            AsyncImage(
+                model = ImageRequest.Builder(context = context)
+                    .data(imageLink.replace("http", "https"))
+                    .crossfade(true)
+                    .build(),
+                contentDescription = stringResource(R.string.book_image),
+//                modifier = Modifier
+//                    .wrapContentSize()
+//                    .fillMaxSize(),
+                placeholder = painterResource(R.drawable.loading_img),
+                error = painterResource(R.drawable.ic_broken_image),
+                alignment = Alignment.Center,
+                contentScale = ContentScale.FillWidth
+            )
+        }
     }
 }
